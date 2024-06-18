dd60f2eb9c87d85f117437baa5fedd919d0702bf
Zakhar <zakhar.alfach@gmail.com>
2024-06-18 23:11:09 +0300

    -basic routing, header layout;

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import HomePage from "@/pages/HomePage.tsx";
+import ProfilePage from "@/pages/ProfilePage.tsx";
+import NotFoundPage from "@/pages/NotFoundPage.tsx";
+import FilmsCatalog from "@/pages/FilmsCatalog.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <NotFoundPage/>,
+        children: [
+            {
+                path: '',
+                element:  <HomePage/>
+            },
+            {
+                path: '/profile',
+                element: <ProfilePage/>
+            },
+            {
+                path: '/catalog',
+                element: <FilmsCatalog/>
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
